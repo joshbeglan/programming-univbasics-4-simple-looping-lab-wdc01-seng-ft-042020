4b0e6aa18b323cb2331dbf05a197230b0626d11e
Josh Beglan <josh_beglan@yahoo.com>
2020-04-16 19:04:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,11 @@
 # Write your methods here
 
 def loop_message_five_times(string)
-  while 
+  i = 0
+  while i < string.length
+    char = string[i]
+    i += 1
+  end
 end
 
 def loop_message_n_times
